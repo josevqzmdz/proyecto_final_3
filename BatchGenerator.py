62051520f5859d54dbbfd75d86c97f6d30da217e
josevqzmdz <notanothergenericaddress@gmail.com>
2022-11-22 05:31:17 -0600

    pag 70

--- BatchGenerator.py
+++ BatchGenerator.py
@@ -1,6 +1,7 @@
 # 64
 import math
-
+import tensorflow as tf
+from keras import optimizers
 # Next, we need a way to iterate over the MNIST data in mini-batches. This is easy:
 
 class BatchGenerator:
@@ -17,3 +18,74 @@ class BatchGenerator:
         labels = self.labels[self.index : self.index + self.batch_size]
         self.index += self.batch_size
         return images, labels
+
+
+    """
+    The most difficult part of the process is the “training step”: updating the weights of
+    the model after running it on one batch of data. We need to
+    1) Compute the predictions of the model for the images in the batch.
+    2) Compute the loss value for these predictions, given the actual labels
+    3) Compute the gradient of the loss with regard to the model’s weights.
+    4) Move the weights by a small amount in the direction opposite to the gradient
+    """
+    # 65
+    def one_training_step(self, model, images_batch, labels_batch):
+        with tf.GradientTape() as tape:
+            predictions = model(images_batch)
+            per_sample_losses = tf.keras.losses.sparse_categorical_crossentropy(
+                labels_batch, predictions
+            )
+            average_loss = tf.reduce_mean(per_sample_losses)
+        """
+        Compute the gradient of the loss with
+        regard to the weights. The output gradients
+        is a list where each entry corresponds to
+        a weight from the model.weights list.
+        """
+        gradients = tape.gradient(average_loss, model.weights)
+        self.update_weights(gradients, model.weights)
+        return average_loss
+
+    """
+    As you already know, the purpose of the “weight update” step 
+    (represented by the pre-
+    ceding update_weights function) is to move the weights by “a bit” 
+    in a direction that
+    will reduce the loss on this batch. The magnitude of the move 
+    is determined by the
+    “learning rate,” typically a small quantity. The simplest way 
+    to implement this
+    update_weights function is to subtract gradient * learning_rate
+     from each weight:
+    """
+    def update_weights(self, gradients, weights):
+        learning_rate = 1e-3
+        for g, w in zip(gradients, weights):
+            # assign_sub is the
+            # equivalent of -= for
+            # TensorFlow variables.
+            w.assign_sub(g * learning_rate)
+
+    # In practice, you would almost never implement a weight update step like this by hand.
+    # Instead, you would use an Optimizer instance from Keras, like this:
+
+    def update_weights_keras(self, gradients, weights):
+        optimizer = optimizers.SGD(learning_rate=1e-3)
+        optimizer.apply_gradients(zip(gradients, weights))
+
+    ## 66
+    """
+    An epoch of training simply consists of repeating the training step for 
+    each batch in
+    the training data, and the full training loop is simply the repetition of 
+    one epoch:
+    """
+    def fit(self, model, images, labels, epochs, batch_size=128):
+        for epoch_counter in range(epochs):
+            print(f"epoch: {epoch_counter}")
+        batch_generator = BatchGenerator(images, labels)
+        for batch_counter in range(batch_generator.num_batches):
+            images_batch, labels_batch = batch_generator.next()
+            loss = self.one_training_step(model, images_batch, labels_batch)
+            if batch_counter % 100 == 0:
+                print(f"loss at batch: {batch_counter}: {loss:.2f}")
--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from NaiveSequential import *
 
 from keras import layers
 
+from BatchGenerator import *
+
 if __name__ == '__main__':
     # 28 francois chollet
     #e = example1()
@@ -21,8 +23,29 @@ if __name__ == '__main__':
     Using this NaiveDense class and this NaiveSequential class, we can create a mock
     Keras model:
     """
+    """
     model = NaiveSequential([
         NaiveDense(input_size = 28 * 28, output_size = 512, activation = tf.nn.relu),
         NaiveDense(input_size = 512, output_size = 10, activation= tf.nn.softmax)
     ])
     assert len(model.weights) == 4
+
+    train_images = train_images.reshape((60000, 28 * 28))
+    train_images = train_images.astype("float32") / 255
+    test_images = test_images.reshape((10000, 28 * 28))
+    test_images = test_images.astype("float32") / 255
+    #batch = BatchGenerator()
+    model.fit(model, train_images, train_labels, epochs=10, batch_size=128)
+    """
+
+    # We can evaluate the model by taking the argmax of its predictions over the test images,
+    # and comparing it to the expected labels:
+    # 66
+    predictions = model(test_images)
+    predictions = predictions.numpy()
+    # Calling .numpy() on a
+    # TensorFlow tensor converts
+    # it to a NumPy tensor
+    predicted_labels = np.argmax(predictions, axis=1)
+    matches = predicted_labels == test_labels
+    print(f"accuracy: {matches.mean(): .2f}")
